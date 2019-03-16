def7bb166760718dca2822e8b5857c8dd27a5ed9
Joshua Carraway <joshuacarraway@Joshuas-MBP.attlocal.net>
2019-03-15 21:58:32 -0500

    giphy

--- logic.js
+++ logic.js
@@ -10,34 +10,34 @@ $(document).ready(function () {
         "Barack Obama", "Dragon Ball Z", "Bahamas", "Jamiaca", "Hawaii", "Cheerleading", "Mad", "Excited"];
 
     console.log("HI");
-    //Creates button based on elements in  the Array
+    
     var newButton = function () {
-        //removes all elements within  the topicBtnSec
+        
         $("#btn-Section").empty();
-        //Creates buttons based on elements in array
+        
         for (var i = 0; i < gifs.length; i++) {
-            //Creates new Button 
+        
             var newBtn = $("<button class=btn-danger>");
-            //give buttons a attribute
+           
             newBtn.attr("data-type", gifs[i]);
-            // add class to button
+           
             newBtn.attr("class", "gif btn-danger");
-            //Give button name that relfxt array
+            
             newBtn.text(gifs[i]);
-            //add button to DOM
+          
             $("#btn-Section").append(newBtn);
 
         }
     }
-    //When subtmit button is clicked
+   
     var submit = function () {
 
         $("#submit-btn").on("click", function (event) {
 
             event.preventDefault();
-            //get input text value
+           
             var Input = $("#userInput").val().trim();
-            //push user input into array
+           
             gifs.push(Input);
 
             newButton();
